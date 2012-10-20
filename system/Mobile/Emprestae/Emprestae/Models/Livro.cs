ceb1bbde5cba8dec51788908b5d5ba7814584485
Renato Pacheco Vieira <renato.pacheco.r2@gmail.com>
2012-10-20 16:16:50 -0400

    Criados Modelos dos Objetos de Resposta do Serviço
    Criado modelo de resposta para resposta dos objetos, livros, filmes e jogos

---
+++ system/Mobile/Emprestae/Emprestae/Models/Livro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Runtime.Serialization;
+
+namespace Emprestae.Models
+{
+    [DataContract]
+    public class Livro
+    {
+        [DataMember]
+        public int idLivro { get; set; }
+
+        [DataMember]
+        public string titulo { get; set; }
+
+        [DataMember]
+        public string autor { get; set; }
+
+        [DataMember]
+        public int edicao { get; set; }
+
+        [DataMember]
+        public string editora { get; set; }
+
+        [DataMember]
+        public string imagePath { get; set; }
+    }
+}
