3f5fb28b222a5ad5e21169749427f645a12aa012
Renato Pacheco Vieira <renato.pacheco.r2@gmail.com>
2012-10-23 22:32:02 -0400

    Atualizando Interface

--- system/Mobile/Emprestae/Emprestae/Pages/Home.xaml.cs
+++ system/Mobile/Emprestae/Emprestae/Pages/Home.xaml.cs
@@ -25,6 +25,12 @@ namespace Emprestae.Pages
         public Home()
         {
             InitializeComponent();
+            searchFriendButton = CreateButton("search", "appbar.feature.search.rest.png");
+            searchObjButton = CreateButton("search", "appbar.feature.search.rest.png");
+            addFriendButton = CreateButton("add", "appbar.add.rest.png");
+            addObjButton = CreateButton("add", "appbar.add.rest.png");
+
+            addObjButton.Click += AddObjButton_OnClick;
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -32,6 +38,11 @@ namespace Emprestae.Pages
             base.OnNavigatedTo(e);
             emprestae.GetUserInfo(success, error);
             emprestae.GetAllUsersButMe(success, error);
+
+            // remove all pages before it
+            while (NavigationService.BackStack.Count() > 0)
+                NavigationService.RemoveBackEntry();
+            
         }
 
         void success(UserResponse response)
@@ -40,7 +51,7 @@ namespace Emprestae.Pages
                 if (response.status != 0)
                 {
                     userPanel.DataContext = response.users[0];
-                    //friendListBox.ItemsSource = response.users;
+                    amigosListBox.ItemsSource = response.users;
                 }
             });
         }
@@ -49,13 +60,56 @@ namespace Emprestae.Pages
         { 
         }
 
-        private void ApplicationBarIconButton_Click(object sender, EventArgs e)
-        {
-            NavigationService.Navigate(new Uri("/Pages/Mapa.xaml",UriKind.Relative));
-        }
-
         private void Panorama_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            int index = (sender as Panorama).SelectedIndex;
+
+            switch (index)
+            {
+                case 1:
+                    ApplicationBar.Buttons.Remove(searchFriendButton);
+                    ApplicationBar.Buttons.Remove(addFriendButton);
+                    ApplicationBar.Buttons.Add(searchObjButton);
+                    ApplicationBar.Buttons.Add(addObjButton);
+                    break;
+                case 2:
+                    ApplicationBar.Buttons.Remove(searchObjButton);
+                    ApplicationBar.Buttons.Remove(addObjButton);
+                    ApplicationBar.Buttons.Add(searchFriendButton);
+                    ApplicationBar.Buttons.Add(addFriendButton);
+                    break;
+                default:
+                    ApplicationBar.Buttons.Remove(searchObjButton);
+                    ApplicationBar.Buttons.Remove(addObjButton);
+                    ApplicationBar.Buttons.Remove(searchFriendButton);
+                    ApplicationBar.Buttons.Remove(addFriendButton);
+                    break;
+            }
         }
+
+        private ApplicationBarIconButton CreateButton(string title, string image)
+        {
+            return new ApplicationBarIconButton()
+            {
+                IsEnabled = true,
+                IconUri = new Uri("/Images/"+image,UriKind.Relative),
+                Text = title
+            };
+        }
+
+        #region Buttons Methods
+
+        private void ApplicationBarIconButton_Click(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Pages/Info.xaml", UriKind.Relative));
+        }
+
+        private void AddObjButton_OnClick(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Pages/AddObjeto.xaml", UriKind.Relative));
+        }
+
+        #endregion
+
     }
 }
